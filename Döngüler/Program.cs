964048fd2db438686bcfd920ce13a827ada36b0e
anilyegitt <anilyegit@gmail.com>
2025-11-28 01:16:41 +0300

    Add files via upload

---
+++ Döngüler/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Döngüler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Console.WriteLine("Hangi konu çalışsın?");
+                Console.WriteLine("1 - For");
+                Console.WriteLine("2 - While Foreach");
+                string secim = Console.ReadLine();
+
+                if (secim == "1")
+                {
+                    var forloop = new ForLoop();
+                    forloop.Run();
+                }
+                else if (secim == "2")
+                {
+                    var whileForeach = new While_Foreach();
+                    whileForeach.Run();
+                }
+                else
+                {
+                    Console.WriteLine("Geçersiz seçim");
+                }
+
+
+                {
+
+                }
+            }
+        }
+    }
+}
+
