8586132ee64d32c4a731c292ff29ab39eb68371f
anilyegitt <anilyegit@gmail.com>
2025-11-27 11:50:21 +0300

    Add files via upload

---
+++ Döngüler/ForLoop.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Döngüler
+{
+    internal class ForLoop
+    {
+
+        public void Run()
+        {
+
+            // Ekrandan girilen sayıya kadar olan tek sayılarını ekrana yaz
+            Console.Write("Lütfen  bir sayı giriniz");
+            int sayac = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= sayac; i++)
+            {
+
+                if (i % 2 == 1)
+                    Console.WriteLine(i);
+            }
+                //1 ile 1000 arasıdaki tek ve çift sayıların kendi içlerinde toplamlatını ekrana yazdır.
+                int tekToplam = 0;
+                int cittToplam = 0;
+
+                for (int i = 1; i <= 1000; i++)
+                {
+                    if (i % 2 == 1)
+                        tekToplam += 1; //tekToplam=tekToplam+i;
+                    else
+                        cittToplam += i;
+
+
+                }
+                Console.WriteLine("Tek Toplam:" + tekToplam);
+                Console.WriteLine("Çift Toplam" + cittToplam);
+
+            //break,continue
+
+            for (int i = 1; i < 10; i++)
+            {
+                if (i == 4)
+                    continue;
+                Console.WriteLine(i); 
+            }
+
+            }
+        }
+    }
+
